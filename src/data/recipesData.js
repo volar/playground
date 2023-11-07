95dba2723f432f1211e1f33123d81d635007d077
Valentin Olar <vali.olare@gmail.com>
2023-11-07 14:29:00 -0500

    fix the data

--- src/data/recipesData.js
+++ src/data/recipesData.js
@@ -1,14 +1,17 @@
-export const recipes = [{
+export const recipes = [
+  {
     id: 'greek-salad',
     name: 'Greek Salad',
-    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta']
-  }, {
+    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta'],
+  },
+  {
     id: 'hawaiian-pizza',
     name: 'Hawaiian Pizza',
-    ingredients: ['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple']
-  }, {
+    ingredients: ['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple'],
+  },
+  {
     id: 'hummus',
     name: 'Hummus',
-    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini']
-  }];
-  
+    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'],
+  },
+];
